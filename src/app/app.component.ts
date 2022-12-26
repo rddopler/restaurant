b4902110eed08ef4a1bd3fd9ccac0ae1c978d6f2
Ryan Dopler <rddopler@gmail.com>
2022-12-26 02:56:01 -0500

    Location Styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {NEBULA} from "./constants/app.constants";
 
 @Component({
   selector: 'app-root',
@@ -7,5 +6,5 @@ import {NEBULA} from "./constants/app.constants";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = NEBULA;
+  title = 'Nebula';
 }
